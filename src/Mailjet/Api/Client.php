9371e5ae17ee23b43652d9b5d337e3d94011b168
Roman Marintšenko <inoryy@gmail.com>
2013-08-07 19:15:53 +0300

    replace 'debug mode' with Symfony Debug Component

--- src/Mailjet/Api/Client.php
+++ src/Mailjet/Api/Client.php
@@ -12,10 +12,6 @@ class Client
     const API_VERSION  = '0.1';
     const API_BASE_URL = 'api.mailjet.com';
 
-    const DEBUG_NONE   = 0;
-    const DEBUG_ERRORS = 1;
-    const DEBUG_ALL    = 2;
-
     const FORMAT_CSV       = 'csv';
     const FORMAT_XML       = 'xml';
     const FORMAT_HTML      = 'html';
@@ -30,14 +26,17 @@ class Client
 
     private $apiKey;
     private $secretKey;
-    private $debugMode      = self::DEBUG_NONE;
     private $outputFormat   = self::FORMAT_ARRAY;
     private $connectionMode = self::CONNECTION_SECURE;
 
-    public function __construct($apiKey, $secretKey)
+    public function __construct($apiKey, $secretKey, $debug = false)
     {
         $this->apiKey    = $apiKey;
         $this->secretKey = $secretKey;
+
+        if ($debug && class_exists('\Symfony\Component\Debug\Debug')) {
+            \Symfony\Component\Debug\Debug::enable();
+        }
     }
 
     /**
@@ -104,26 +103,6 @@ class Client
     public function getConnectionMode()
     {
         return $this->connectionMode;
-    }
-
-    public function setDebugMode($debugMode)
-    {
-        if (!in_array($debugMode, array(
-            self::DEBUG_NONE,
-            self::DEBUG_ERRORS,
-            self::DEBUG_ALL
-        ))) {
-            throw new \InvalidArgumentException('Unsupported debug mode: ' . $debugMode);
-        }
-
-        $this->debugMode = $debugMode;
-
-        return $this;
-    }
-
-    public function getDebugMode()
-    {
-        return $this->debugMode;
     }
 
     public function setOutputFormat($outputFormat)
