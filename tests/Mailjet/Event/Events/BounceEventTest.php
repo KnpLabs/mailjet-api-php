6a1319f613d0f94e4db6cd1e730d37c6afc21e46
Roman Marintšenko <inoryy@gmail.com>
2013-08-29 21:35:25 +0300

    add test coverage for BounceEvent

---
+++ tests/Mailjet/Event/Events/BounceEventTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mailjet\Tests\Event\Events;
+
+use Mailjet\Event\Data\EventData;
+use Mailjet\Event\Events\BounceEvent;
+
+class BounceEventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $data = array(
+            EventData::DATA_EVENT       => EventData::EVENT_BLOCKED,
+            EventData::DATA_BLOCKED     => true,
+            EventData::DATA_HARD_BOUNCE => false
+        );
+
+        $event = new BounceEvent($data);
+
+        $this->assertEquals($event->isBlocked(),    $data[EventData::DATA_BLOCKED]);
+        $this->assertEquals($event->isHardBounce(), $data[EventData::DATA_HARD_BOUNCE]);
+    }
+}
