06eaf3e7b1f0daafd602c6c08c5a92d4c54308e5
Roman Marintšenko <inoryy@gmail.com>
2013-08-29 21:31:40 +0300

    fix bug in OpenEvent getters

--- src/Mailjet/Event/Events/OpenEvent.php
+++ src/Mailjet/Event/Events/OpenEvent.php
@@ -8,12 +8,12 @@ class OpenEvent extends EmailEvent
 {
     public function getGeo()
     {
-        return $this->data[EventData::DATA_IP];
+        return $this->data[EventData::DATA_GEO];
     }
 
     public function getIp()
     {
-        return $this->data[EventData::DATA_GEO];
+        return $this->data[EventData::DATA_IP];
     }
 
     public function getUserAgent()
