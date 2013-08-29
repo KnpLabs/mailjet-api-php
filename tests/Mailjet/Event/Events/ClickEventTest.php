9ebf37e246dbec6187c7be4359eed85090c3cc9f
Roman Marintšenko <inoryy@gmail.com>
2013-08-29 21:36:49 +0300

    add test coverage for ClickEvent

---
+++ tests/Mailjet/Event/Events/ClickEventTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Mailjet\Tests\Event\Events;
+
+use Mailjet\Event\Data\EventData;
+use Mailjet\Event\Events\ClickEvent;
+
+class ClickEventTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreate()
+    {
+        $data = array(
+            EventData::DATA_EVENT => EventData::EVENT_BLOCKED,
+            EventData::DATA_URL   => 'http://example.com',
+        );
+
+        $event = new ClickEvent($data);
+
+        $this->assertEquals($event->getUrl(), $data[EventData::DATA_URL]);
+    }
+}
